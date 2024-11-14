a9aaea2dc1473828de3b4034331e86152721d252
Sergio <8042sergi@gmail.com>
2024-11-14 16:14:19 +0100

    fix bugs

--- src/assets/index.js
+++ src/assets/index.js
@@ -29,7 +29,7 @@ import octupus from "./company/octupus.png";
 import soaint from "./company/soaint.png";
 import mome from "./company/mome.png";
 import carrent from "./carrent.png";
-import nextjsLogoo from "./nextjsLogoo.png";
+import nextjsLogo from "./nextjsLogo.png";
 import webdevelopment from "./webdevelopment.png";
 import jobit from "./jobit.png";
 import reactnative from "./reactnative.png";
@@ -43,7 +43,7 @@ export {
   webdevelopment,
   mobile,
   web,
-  nextjsLogoo,
+  nextjsLogo,
   github,
   menu,
   close,
